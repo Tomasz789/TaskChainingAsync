9478099ae80e732b2b2cbe2a19a01925b25deacd
Tomasz789 <lachowiec_95@wp.pl>
2022-06-16 21:46:18 +0200

    Program implemented

--- TaskChaining/Program.cs
+++ TaskChaining/Program.cs
@@ -12,10 +12,34 @@ namespace TaskChaining
         static async Task Main(string[] args)
         {
             var rnd = new Random();
+            var taskClass = new TaskChainingClass();
             int min = 0, max = 1;
 
-            Console.WriteLine("Generating an array of integers: ");
+            do
+            {
+                min = rnd.Next();
+                max = rnd.Next();
+            }
+            while (min >= max);
 
+            Console.WriteLine("Generating an array of integers: ");
+            var generatedArray = taskClass.GenerateIntegerArray(10, min, max).Result;
+            Console.WriteLine("Generated values: ");
+            PrintCurrentArrayValues(generatedArray);
+            Console.WriteLine("Average value: {0}", taskClass.GetAverageValue(generatedArray).Result);
+            int mult = rnd.Next();
+            Console.WriteLine("Array multiplied by: {0}", mult);
+            Console.WriteLine();
+            var multArray = taskClass.ReturnMultipliedArray(generatedArray, mult).Result;
+            PrintCurrentArrayValues(multArray);
+            Console.WriteLine("Average value: {0}", taskClass.GetAverageValue(multArray).Result);
+            Console.WriteLine();
+            Console.WriteLine("Array after sorting:");
+            var sortedArray = taskClass.SortArrayAscending(multArray).Result;
+            PrintCurrentArrayValues(sortedArray);
+            Console.WriteLine("Average value: {0}", taskClass.GetAverageValue(sortedArray).Result);
+
+            await Task.CompletedTask;
 
         }
 
